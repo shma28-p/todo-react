0ff267018594f1a26d92f56139789cd8e2009c7a
shma28-p <108649018+shma28-p@users.noreply.github.com>
2022-07-05 14:50:31 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,71 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
 
 export const App = () => {
+  // state セット
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState(["aaa", "bbb"]);
+  const [completeTodos, setCompleteTodos] = useState(["ccc"]);
+
+  // インプットエリア処理周り
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  // 未完了リストから削除
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  // 未完了リスト -> 完了リスト
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    setIncompleteTodos(newIncompleteTodos);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  // 完了リスト -> 未完了リスト
+  const onClickBack = (index) => {
+    // 変更後の完了リストの配列作成
+    console.log(completeTodos[index]);
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    setCompleteTodos(newCompleteTodos);
+    // 変更後の未完了リストの配列作成
+    console.log(completeTodos);
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+  };
+
   return (
     <>
-      <div>
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
-      </div>
-      <div>
-        <p>未完了のTODO</p>
-        <ul>
-          <li>
-            <div>
-              <p>aaa</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <p>完了したTODO</p>
-        <ul>
-          <li>
-            <div>
-              <p>bbb</p>
-              <button>戻す</button>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
+      />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるtodoは5個まで</p>
+      )}
+      <IncompleteTodos
+        incompleteTodos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos completeTodos={completeTodos} onClickBack={onClickBack} />
     </>
   );
 };
---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const CompleteTodos = (props) => {
+  const { completeTodos, onClickBack } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了したTODO</p>
+      <ul>
+        {completeTodos.map((todo, index) => {
+          return (
+            <li key={index}>
+              <div className="list-row">
+                <p>{todo}</p>
+                <button onClick={() => onClickBack(index)}>戻す</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
